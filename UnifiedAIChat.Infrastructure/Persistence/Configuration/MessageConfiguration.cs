8427024b4c99211accbdcfc3db9df55089ebf071
German Takhirov <gleb200360@gmail.com>
2026-04-29 19:14:46 +0900

    add: Message Fluent API

---
+++ UnifiedAIChat.Infrastructure/Persistence/Configuration/MessageConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnifiedAIChat.Core.Models;
+
+namespace UnifiedAIChat.Infrastructure.Persistence.Configuration
+{
+    internal class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasKey(m => m.Id);
+
+            builder.HasKey(m => m.ChatId);
+
+            builder.Property(m => m.Content).IsRequired();
+
+            builder.Property(m => m.Role).HasConversion<string>().HasMaxLength(20).IsRequired();
+
+            builder.HasOne(m => m.Chat).WithMany(c => c.Messages).HasForeignKey(m => m.ChatId).OnDelete(DeleteBehavior.Cascade);
+
+        }
+
+    }
+}
